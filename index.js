4d7d78039d744b0eb7e5aaa5b92d52217e0c51a6
Matper17 <matiasperalta992@gmail.com>
2023-09-18 23:16:47 -0300

    Correcciones desafío2

---
+++ index.js
@@ -0,0 +1,14 @@
+const products =  [
+    {
+        name: "product1", 
+        stock: 230
+    },
+    {
+        name: "product2", 
+        stock: 100
+    },
+    {
+        name: "product3", 
+        stock: 100
+    }
+]
